2db46cfccd41b1d08624004f63a0a447d89fd028
MrFlexi <szaroletta@hotmail.com>
2019-05-29 18:16:09 +0200

    Alive Counter

--- HelloWorld/HelloWorld.ino
+++ HelloWorld/HelloWorld.ino
@@ -2,7 +2,10 @@
 #include <hal/hal.h>
 #include <SPI.h>
 #include <U8g2lib.h>
+#include <Ticker.h>
 
+
+Ticker aliveTicker;
 
 // LoRa Pins
 #define LoRa_RST  14  // GPIO 14
@@ -12,27 +15,24 @@
 #define LoRa_DIO2 32  // GPIO 32
 
 int runmode = 0;
+int aliveCounter = 0;
+String stringOne = "";
 
 // assume 4x6 font, define width and height
 #define U8LOG_WIDTH 32
 #define U8LOG_HEIGHT 10
 
 
-// This EUI must be in little-endian format, so least-significant-byte
-// first. When copying an EUI from ttnctl output, this means to reverse
-// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
-// 0x70.
+// Copy the value from Application EUI from the TTN console in LSB mode
 static const u1_t PROGMEM APPEUI[8]={ 0x4B, 0xCC, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70  };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
-// This should also be in little endian format, see above.
+// Copy the value from Device EUI from the TTN console in LSB mode.
 static const u1_t PROGMEM DEVEUI[8]={ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xDE  };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
-// This key should be in big endian format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = { 0x01, 0x11, 0x2D, 0x27, 0x16, 0x37, 0x7C, 0x53, 0xA3, 0xF6, 0x1C, 0x1F, 0x46, 0xD3, 0x48, 0x0C };
+// Anyway its in MSB mode.
+static const u1_t PROGMEM APPKEY[16] = { 0xA6, 0x6B, 0xB1, 0x34, 0xEC, 0xB5, 0x2F, 0xA1, 0x53, 0x7A, 0xBC, 0x70, 0xB5, 0x9F, 0xCF, 0x7F };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static uint8_t mydata[] = "Hello, world!";
@@ -41,6 +41,7 @@ static osjob_t sendjob;
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
 const unsigned TX_INTERVAL = 60;
+const float alivePeriod = 30; //seconds
 
 
 
@@ -78,9 +79,18 @@ void log_display(String s)
   }
 }
 
+
+void alive() {
+  aliveCounter++;
+  stringOne = "Alive: ";
+  stringOne = stringOne + aliveCounter;   
+  log_display(stringOne);  
+}
+
+
 void onEvent (ev_t ev) {
     Serial.print(os_getTime());
-    Serial.print(": ");
+    Serial.print(": ");    
     switch(ev) {
         case EV_SCAN_TIMEOUT:
             Serial.println(F("EV_SCAN_TIMEOUT"));
@@ -91,17 +101,17 @@ void onEvent (ev_t ev) {
             log_display("Beacon found");
             break;
         case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
+            log_display("EV_BEACON_MISSED");
             break;
         case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
+            log_display("EV_BEACON_TRACKED");
             break;
         case EV_JOINING:
             Serial.println(F("EV_JOINING"));
             log_display("Joining...");
             break;
         case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
+            log_display("EV_JOINED");
             {
               u4_t netid = 0;
               devaddr_t devaddr = 0;
@@ -109,7 +119,7 @@ void onEvent (ev_t ev) {
               u1_t artKey[16];
               LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
               log_display("connected");
-              Serial.print("netid: ");
+              log_display("netid: ");
               Serial.println(netid, DEC);
               Serial.print("devaddr: ");
               Serial.println(devaddr, HEX);
@@ -138,13 +148,13 @@ void onEvent (ev_t ev) {
         ||     break;
         */
         case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
+            log_display("EV_JOIN_FAILED");
             break;
         case EV_REJOIN_FAILED:
             Serial.println(F("EV_REJOIN_FAILED"));
             break;
         case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            log_display("EV_TXCOMPLETE");
             if (LMIC.txrxFlags & TXRX_ACK)
               Serial.println(F("Received ack"));
             if (LMIC.dataLen) {
@@ -156,20 +166,20 @@ void onEvent (ev_t ev) {
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
+            log_display("EV_LOST_TSYNC");
             break;
         case EV_RESET:
-            Serial.println(F("EV_RESET"));
+           log_display("EV_RESET");
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
+            log_display("EV_RXCOMPLETE");
             break;
         case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
+            log_display("EV_LINK_DEAD");
             break;
         case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
+            log_display("EV_LINK_ALIVE");
             break;
         /*
         || This event is defined but not used in the code. No
@@ -180,10 +190,10 @@ void onEvent (ev_t ev) {
         ||    break;
         */
         case EV_TXSTART:
-            Serial.println(F("EV_TXSTART"));
+            log_display("EV_TXSTART");
             break;
         default:
-            Serial.print(F("Unknown event: "));
+            log_display("Unknown event: ");
             Serial.println((unsigned) ev);
             break;
     }
@@ -192,11 +202,11 @@ void onEvent (ev_t ev) {
 void do_send(osjob_t* j){
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        log_display("OP_TXRXPEND, not sending");
     } else {
         // Prepare upstream data transmission at the next possible time.
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
-        Serial.println(F("Packet queued"));
+       log_display("Packet queued");
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
@@ -211,7 +221,7 @@ void setup_display(void)
   u8g2.enableUTF8Print();
   u8g2log.print("Display loaded...");
   u8g2log.print("\n");
-  u8g2log.print("Terminal mode");
+  u8g2log.print("TTN - MQTT");
   u8g2log.print("\n");
 }
 
@@ -220,20 +230,49 @@ void setup() {
     Serial.println(F("Starting"));
     setup_display();
 
-    #ifdef VCC_ENABLE
-    // For Pinoccio Scout boards
-    pinMode(VCC_ENABLE, OUTPUT);
-    digitalWrite(VCC_ENABLE, HIGH);
-    delay(1000);
-    #endif
+    aliveTicker.attach(alivePeriod, alive);    
 
     // LMIC init
     os_init();
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
+    
+    LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    //LMIC_setDrTxpow(DR_SF11,14);
+    LMIC_setDrTxpow(DR_SF9,14);
 
     // Start job (sending automatically starts OTAA too)
-    do_send(&sendjob);
+    do_send(&sendjob);     // Will fire up also the join
 }
 
 void loop() {
